d601eab90ffcf3f862318043d0798a2a220c7e9e
icarmonag <31824460+icarmonag@users.noreply.github.com>
2017-10-29 17:18:55 -0600

    Initial Commit

---
+++ TiendaDeZapatos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TiendaDeZapatos
+//
+//  Created by Ivan on 10/29/17.
+//  Copyright © 2017 wondererApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TiendaDeZapatos/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TiendaDeZapatos
+//
+//  Created by Ivan on 10/29/17.
+//  Copyright © 2017 wondererApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
